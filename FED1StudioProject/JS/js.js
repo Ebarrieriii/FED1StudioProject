06f24bc86124bcb6473ecb4c3ca9a82ea2c6778a
edwardiiibarrier@gmail.com <edwardiiibarrier@gmail.com>
2021-04-24 15:52:37 -0500

    Almost complete

--- FED1StudioProject/JS/js.js
+++ FED1StudioProject/JS/js.js
@@ -10,3 +10,11 @@
         btnHamburger.innerHTML = "&#9776;";
     }
 }
+
+    var today = new Date();
+    var year = today.getFullYear();
+
+    var el = document.getElementById('footerp');
+    el.innerHTML = 'All Rights Reserved ' + year;
+
+
